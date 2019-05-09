c69899729b290b405fdd1d235c00ceda5a0df51c
parth purani <parthpurani1997@gmail.com>
2019-05-09 22:39:04 +0530

    patch

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,re_path
-from . import views
-
-urlpatterns = [
-    re_path('^$', views.index, name = 'index'),
-    re_path('^home/', views.index, name = 'index')
-
-]
