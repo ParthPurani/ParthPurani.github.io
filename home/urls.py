573b8403766aab8a1c9a44ab77ddd3b2c7976291
parth purani <parthpurani1997@gmail.com>
2019-05-09 22:17:23 +0530

    Initial commit
    initial commit

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,re_path
+from . import views
+
+urlpatterns = [
+    re_path('^$', views.index, name = 'index'),
+    re_path('^home/', views.index, name = 'index')
+
+]
